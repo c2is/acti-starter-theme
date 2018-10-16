e6b3136014c2a1b44d6fc3f82a10cd0a2de24b77
a.delcroix <a.delcroix@acti.fr>
2018-10-16 17:16:24 +0200

    Create capaibilites for theme general settings Create functions to remove capabilities in capabilites helper

--- functions/capabilities.php
+++ functions/capabilities.php
@@ -10,5 +10,11 @@ function acti_administrator_capabilities()
     $role = get_role('administrator');
 
     $capabilitesManager = new \Capabilities\Capabilities();
-    $capabilitesManager::addCap('edit_theme_general_settings', $role);
+
+    /* Capabilies to edit admin general theme settings and others */
+    $actiCaps = array(
+        'edit_theme_settings',
+        'edit_theme_general_settings'
+    );
+    $capabilitesManager::addCaps($actiCaps, $role);
 }
--- helpers/Capabilities/Capabilities.php
+++ helpers/Capabilities/Capabilities.php
@@ -5,10 +5,25 @@ namespace Capabilities;
 final class Capabilities
 {
     /**
-     * @param $postTypeSlug
+     * @param $postTypeSlug string
      * @param $role \WP_Role role to receive new capability
      */
     public static function addPostTypeCaps($postTypeSlug, $role)
+    {
+        $role->add_cap('read_' . $postTypeSlug);
+        $role->add_cap('delete_' . $postTypeSlug);
+        $role->add_cap('edit_' . $postTypeSlug . 's');
+        $role->add_cap('edit_others_' . $postTypeSlug . 's');
+        $role->add_cap('publish_' . $postTypeSlug . 's');
+        $role->add_cap('read_private_' . $postTypeSlug . 's');
+        $role->add_cap('edit_' . $postTypeSlug . 's');
+    }
+
+    /**
+     * @param $postTypeSlug string
+     * @param $role \WP_Role role to drop capability
+     */
+    public static function removePostTypeCaps($postTypeSlug, $role)
     {
         $role->add_cap('read_' . $postTypeSlug);
         $role->add_cap('delete_' . $postTypeSlug);
@@ -29,6 +44,15 @@ final class Capabilities
     }
 
     /**
+     * @param $cap string capabilty to remove
+     * @param $role \WP_Role role to drop capability
+     */
+    public static function removeCap($cap, $role)
+    {
+        $role->remove_cap($cap);
+    }
+
+    /**
      * @param $caps array capabilties to add
      * @param $role \WP_Role role to receive new capability
      */
@@ -38,6 +62,20 @@ final class Capabilities
         {
             foreach ($caps as $cap) {
                 $role->add_cap($cap);
+            }
+        }
+    }
+
+    /**
+     * @param $caps array capabilties to add
+     * @param $role \WP_Role role to drop capability
+     */
+    public static function removeCaps($caps, $role)
+    {
+        if (!empty($caps))
+        {
+            foreach ($caps as $cap) {
+                $role->remove_cap($cap);
             }
         }
     }
