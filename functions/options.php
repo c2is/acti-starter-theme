c68c23ea77433b168d2b547b57c8be2ca7db2203
a.delcroix <a.delcroix@acti.fr>
2018-10-16 17:15:21 +0200

    Add admin menu theme options

---
+++ functions/options.php
@@ -0,0 +1,24 @@
+<?php
+
+/* Create ACF global theme options */
+if (function_exists('acf_add_options_page'))
+{
+    /* Admin menu general settings */
+    $optionPageArgs = array(
+        'page_title' => 'Options générales',
+        'menu_title' => 'Options générales',
+        'menu_slug' => 'theme-general-settings',
+        'capability' => 'edit_theme_settings',
+    );
+    acf_add_options_page($optionPageArgs);
+
+    /* Admin submenu theme settings */
+    $themeOptionPageArgs = array(
+        'page_title' => 'Options générales du thème',
+        'menu_title' => 'Thème',
+        'parent_slug' => 'theme-general-settings',
+        'capability' => 'edit_theme_general_settings',
+    );
+    acf_add_options_sub_page($themeOptionPageArgs);
+}
+
