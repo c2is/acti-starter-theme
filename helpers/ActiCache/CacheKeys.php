4a34cda7c3d898cfc6d04c193f5a3335b76b9aac
a.delcroix <a.delcroix@acti.fr>
2018-10-25 15:42:25 +0200

    Add post cache key

--- helpers/ActiCache/CacheKeys.php
+++ helpers/ActiCache/CacheKeys.php
@@ -6,10 +6,19 @@ final class CacheKeys
 {
     /**
      * @param $postId int post ID
+     * @return string post base cache key
+     */
+    public static function getPostBaseCacheKey($postId)
+    {
+        return get_locale() . '/post-' . $postId . '/';
+    }
+
+    /**
+     * @param $postId int post ID
      * @return String block base cache key
      */
     public static function getLayoutBaseCacheKey($postId)
     {
-        return get_locale() . '/post-' . $postId . '/layout/';
+        return self::getPostBaseCacheKey($postId) . 'layout/';
     }
 }
