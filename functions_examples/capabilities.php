f7c27efdabacd6b454fe68a643a095886419357b
a.delcroix <a.delcroix@acti.fr>
2018-10-25 16:13:21 +0200

    Use capabilities helper as static class

--- functions_examples/capabilities.php
+++ functions_examples/capabilities.php
@@ -9,15 +9,13 @@ function administrator_capabilities()
 {
     $role = get_role('administrator');
 
-    $capabilitesManager = new \Capabilities\Capabilities();
-
     /* Add caps to allow role to manage 'acti' post type */
-    $capabilitesManager::addPostTypeCaps('acti', $role);
+    \Capabilities\Capabilities::addPostTypeCaps('acti', $role);
 
     /* Add single cap to role */
-    $capabilitesManager::addCap('custom_cap', $role);
+    \Capabilities\Capabilities::addCap('custom_cap', $role);
 
     /* Add multiple caps to role */
     $caps = array('cap1', 'cap2', 'cap3');
-    $capabilitesManager::addCaps($caps, $role);
+    \Capabilities\Capabilities::addCaps($caps, $role);
 }
