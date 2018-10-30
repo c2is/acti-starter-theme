86c0501d95b79b4432420273008e53405181dc63
a.delcroix <a.delcroix@acti.fr>
2018-10-30 09:37:50 +0100

    Change capabilities for post types

--- helpers/Capabilities/Capabilities.php
+++ helpers/Capabilities/Capabilities.php
@@ -5,18 +5,42 @@ namespace Capabilities;
 final class Capabilities
 {
     /**
+     * Capabilites to only edit or create own post type
+     *
+     * @param $postTypeSlug string
+     * @param $role \WP_Role role to receive new capability
+     */
+    public static function addOwnPostTypeCaps($postTypeSlug, $role)
+    {
+        $role->add_cap('read_' . $postTypeSlug);
+        $role->add_cap('read_private_' . $postTypeSlug . 's');
+        $role->add_cap('delete_' . $postTypeSlug);
+        $role->add_cap('edit_' . $postTypeSlug);
+        $role->add_cap('edit_' . $postTypeSlug . 's');
+        $role->add_cap('edit_private_' . $postTypeSlug . 's');
+        $role->add_cap('edit_published_' . $postTypeSlug . 's');
+    }
+
+    /**
+     * Capabilites to edit, create, delete or publish post type
+     *
      * @param $postTypeSlug string
      * @param $role \WP_Role role to receive new capability
      */
     public static function addPostTypeCaps($postTypeSlug, $role)
     {
         $role->add_cap('read_' . $postTypeSlug);
-        $role->add_cap('delete_' . $postTypeSlug);
-        $role->add_cap('edit_' . $postTypeSlug . 's');
-        $role->add_cap('edit_others_' . $postTypeSlug . 's');
-        $role->add_cap('publish_' . $postTypeSlug . 's');
         $role->add_cap('read_private_' . $postTypeSlug . 's');
+        $role->add_cap('edit_' . $postTypeSlug);
         $role->add_cap('edit_' . $postTypeSlug . 's');
+        $role->add_cap('edit_private_' . $postTypeSlug . 's');
+        $role->add_cap('edit_others_' . $postTypeSlug . 's');
+        $role->add_cap('edit_published_' . $postTypeSlug . 's');
+        $role->add_cap('delete_' . $postTypeSlug);
+        $role->add_cap('delete_private_' . $postTypeSlug . 's');
+        $role->add_cap('delete_published_' . $postTypeSlug . 's');
+        $role->add_cap('delete_others_' . $postTypeSlug . 's');
+        $role->add_cap('publish_' . $postTypeSlug . 's');
     }
 
     /**
